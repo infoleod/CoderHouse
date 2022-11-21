41dc00087ea3e1446cf130945c4e6535a8ae9ecd
infoleod <infoleod@gmail.com>
2022-11-21 20:30:43 -0300

    Comit clase 0-1

--- clase1/clase_uno.js
+++ clase1/clase_uno.js
@@ -1,3 +1,9 @@
-let variable = 'Abunda La Caca !!!';
+let gato = prompt("ingrese el nombre del gato");
 
-
+if (gato == "ramon") {
+    alert("miiiiiiiiiiiiiiiiiiii");
+} else if (gato == 'mia') {
+    alert("yo soy calentura pura...");
+} else if (gato == 'ambar') {
+    alert("que carajo usas tu nombre la concha de tu madre..");
+}
